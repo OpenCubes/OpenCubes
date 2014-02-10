afe138533b6b9969fa53c4ae81ad810a3dd67c08
Vinz243 <Vinz243@gmail.com>
2014-02-10 18:35:38 +0100

    port

--- server/config.js
+++ server/config.js
@@ -5,6 +5,6 @@ module.exports = {
         'user': 'server',
         'pass': 'faae5288a8a0e9f986d9e496926beb55a7e9c041'
     },
-    port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000,
-    ip: process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1'
+    port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000,
+    ip: process.env.OPENSHIFT_NODEJS_IP || process.env.IP || undefined
 };
