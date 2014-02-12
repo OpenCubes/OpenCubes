3f4e3706a570a7f3675c433d2c92aaf8df6551fa
Vinz243 <Vinz243@gmail.com>
2014-02-12 16:31:39 +0100

    added laoding spinners

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,25 @@
 // Dynamically bining event for links to come 
-$(document).on('click', 'a[data-ajax=true]', function(event) {
-    event.preventDefault();
-    history.pushState({}, "page 2", $(this).attr('href'));
-    $('.screen').css({display: 'none'});   
-    $('.screen').fadeIn();
-    $('html, body').animate({scrollTop:0}, 'slow');
+$(document).on('click', 'a[data-ajax=true]', function (event) {
+	event.preventDefault();
+	history.pushState({}, "page 2", $(this).attr('href'));
+	/*$('.screen').css({
+		'display': 'block'
+	});*/
+	$('.screen').fadeIn();
+	$('html, body').animate({
+		scrollTop: 0
+	}, 'slow');
 
-    $.ajax({
-        url: $(this).attr('href') + '?ajax=true',
-        success: function (data) {
-            $('#main').html(data.body);
-            $('.screen').fadeOut();
-        }
-    })
+	$.ajax({
+		url: $(this).attr('href') + '?ajax=true',
+		success: function (data) {
+			$('#main').html(data.body);
+			$('.screen').fadeOut(function () {
+				$('.screen').css({
+					display: 'none'
+				});
+			});
+
+		}
+	})
 });
