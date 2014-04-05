d957677be283c3e387091f0faf476080bf9996c7
Vinz243 <Vinz243@gmail.com>
2014-04-05 16:31:30 +0200

    added joconut

--- public/js/main.js
+++ public/js/main.js
@@ -1,54 +1,9 @@
 (function () {
-	var isFetchingNow = false;
-	$.joconut.on('error', function (err) { // fires on timeout, page without <body>, invalid requests
-		console.log(err.state());
-		if (err.status === 401) showLoginPrompt('Please login first and retry', 'danger');
-		else $('#main').prepend('<div class="panel panel-danger"><div class="panel-heading"><h3 class="panel-title pull-left">' + 'There was an error fetching new page (' + err.message + ')</h3>' + '<button type="button" class="close pull-right" aria-hidden="true" onclick="$(\'.panel-danger\').remove()">' + '&times;</button><div class="clearfix col-x"></div></div></div>');
-		stopProgress();
-		isFetchingNow = false;
-
-
-	});
-
-	$.joconut.on('fetch', function () { // Page changed
-
-		console.log('fetched');
-		triggerLoad();
-		isFetchingNow = false;
-	});
-
-	$.joconut.on('before:fetch', function () { // page will be loaded now
-		if (!isFetchingNow) {
-			console.log('fetching')
-			startProgress();
-			isFetchingNow = true;
-		}
-
-
-	});
-	var startProgress = function () {
-		$('.screen').fadeIn(100);
-		$('html, body').animate({
-			scrollTop: 0
-		}, 'fast');
-		$('#main').animate({
-			opacity: 0.5
-		}, 'fast');
-	}
-	var stopProgress = function () {
-		$('.screen').fadeOut(100, function () {
-			$('.screen').css({
-				display: 'none'
-			});
-			$('#main').animate({
-				opacity: 1
-			}, 'fast');
-		});
-	}
 
 
 })();
 $(document).ready((triggerLoad = function () {
+  InstantClick.init();
 	if ($.fn.markdown) {
 
 		$('.markdown-enabled').markdown({
--- server/config.js
+++ server/config.js
@@ -1,10 +1,10 @@
 (function() {
   module.exports = {
     securitySalt: "&*5/5+hd-5qkjzn;?66@-{]=}",
-    db_uri: "mongodb://troup.mongohq.com:10045/opencubes",
+    db_uri: "mongodb://oceanic.mongohq.com:10060/local-dev",
     db_opt: {
       user: "server",
-      pass: "faae5288a8a0e9f986d9e496926beb55a7e9c041"
+      pass: "2f61cb7837b83df50a7fffb58e802b87679cdaff"
     },
     port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000,
     ip: process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "",
