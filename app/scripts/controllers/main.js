2ff6e6f2fa045e3c67675fbdaaa5d69def1286c5
Vinz243 <vinz243@gmail.com>
2014-06-18 14:50:58 +0200

    Init AngularJS

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dashboardApp
+ */
+angular.module('dashboardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
